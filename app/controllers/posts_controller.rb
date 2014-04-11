ccad560690a3bdcf84fc6bbeaa0f65582aab993e
tommotaylor <tommotaylor@gmail.com>
2014-04-11 19:07:52 +0100

    Start of lesson two, stuck on getting new posts to save from form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,42 @@
 class PostsController < ApplicationController
+  
   def index
-
+    @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new(params[:id])
+  end
+
+  def create
+    @post = Post.new(params[:id])
+      if @post.save
+        flash[:notice] = "Successfully saved"
+        redirect_to 'posts'
+      else
+        flash[:error] = "There was an error"
+        render 'new'
+      end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+
+
+private
+
+  
+
+
+
+
 end
--- app/models/concerns/post.rb
+++ app/models/concerns/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base;
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :comments
+
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
